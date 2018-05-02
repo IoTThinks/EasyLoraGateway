324c4c4d0d1eca75bffefe7c1d538e347d15f418
IoTThinks.com <vinhdat82@gmail.com>
2018-05-02 19:29:38 +0700

    Added comments

--- EasyLoRaNode/EasyLoRaNode.ino
+++ EasyLoRaNode/EasyLoRaNode.ino
@@ -6,6 +6,7 @@
 #include <Wire.h>  
 #include "SSD1306.h"
 
+// To map ESP32's PIN to LoRa module
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISO
 #define MOSI    27   // GPIO27 -- SX1278's MOSI
@@ -14,28 +15,41 @@
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND    433E6
 
+// Temp, to count sent messages
 unsigned int counter = 0;
 
+// Init OLED
 SSD1306 display(0x3c, 4, 15);
+
+// Packet content
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
 void setup() {
+  // Init OLED
   pinMode(16,OUTPUT);
   pinMode(25,OUTPUT);
   
   digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
   digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
-  
+
+  // Init Serial
   Serial.begin(9600);
   while (!Serial);
+
+  // Sending LoRa message
   Serial.println();
   Serial.println("LoRa Sender Test");
-  
+
+  // Init LoRa module via SPI in ESP32
   SPI.begin(SCK,MISO,MOSI,SS);
+
+  // Set LoRa's most important PINs
   LoRa.setPins(SS,RST,DI0);
+
+  // Set frequency for LoRa
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
@@ -43,6 +57,8 @@ void setup() {
   
   //  LoRa.onReceive(cbk);
   //  LoRa.receive();
+
+  // Init the OLED and font
   Serial.println("init ok");
   display.init();
   display.flipScreenVertically();  
@@ -51,21 +67,26 @@ void setup() {
 }
 
 void loop() {
+  // Clear OLED and set font
   display.clear();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
-  
+
+  // Display sending LoRa packet
   display.drawString(0, 0, "Sending packet: ");
   display.drawString(90, 0, String(counter));
   display.display();
 
-  // send packet
+  // Sending LoRa packet as Hello 1, Hello 2...
   LoRa.beginPacket();
-  LoRa.print("hello ");
+  LoRa.print("Hello ");
   LoRa.print(counter);
   LoRa.endPacket();
 
+  // Increase counter for next message.
   counter++;
+
+  // Blink the LED for each LoRa send
   digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
