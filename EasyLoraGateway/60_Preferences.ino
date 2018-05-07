f036a85af6c794f21ae8439419a1700c4ad0cbd0
IoTThinks.com <vinhdat82@gmail.com>
2018-05-07 19:52:47 +0700

    Init codes to save Preferences

---
+++ EasyLoraGateway/60_Preferences.ino
@@ -0,0 +1,6 @@
+// ====================================
+// PREFERENCES
+// ====================================
+// To save preferences to flash (without SD card)
+// https://github.com/espressif/arduino-esp32/blob/master/libraries/Preferences/examples/StartCounter/StartCounter.ino
+// http://www.iotsharing.com/2017/06/how-to-use-preferences-to-backup-data-in-main-flash-when-power-off.html?m=1
